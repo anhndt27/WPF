8e272d50518b54d35b28f6a5f57128e45d8e81ce
anhndt27 <anhndt27@gmail.com>
2024-07-11 15:32:20 +0700

    first init wpf app

---
+++ View/Utils/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace View.Utils
+{
+    class Helpers
+    {
+        public static BitmapImage ConvertByteToImageBitmap(byte[] array)
+        {
+            MemoryStream ms = new MemoryStream(array);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+        public static byte[] ConvertImageToByte(BitmapImage image)
+        {
+            var encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(image));
+            var ms = new MemoryStream();
+            encoder.Save(ms);
+            return ms.ToArray();
+        }
+    }
+}
